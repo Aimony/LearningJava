3a10259f8f31303259895c8d11016d7f44fc47cb
Aimony <2249398422@qq.com>
2022-02-24 18:58:53 +0800

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int age = 18;
+        double heigh = 179.9;
+        float weight = 55.6F;
+
+        System.out.println(age);
+        System.out.println(heigh);
+        System.out.println(weight);
+    }
+}
